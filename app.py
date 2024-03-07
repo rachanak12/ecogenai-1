64e8f474d63e36ec20942573a89c6161215565a6
Abhiram <113409577+Controls-Cat5e@users.noreply.github.com>
2024-03-07 00:39:52 +0000

    fixed  api

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import plotly.express as px
-palm.configure(api_key='AIzaSyATRWBWwqP1AYY1gNJEHvKPKSBWorFABv8') # need to remove/hide
+palm.configure(api_key=st.secrets["api_key"]) # need to remove/hide
 df = pd.read_csv("datafinal.csv")
 
 models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
